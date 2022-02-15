8c0838479f91cbb1a6d8cfc07a012493ecfac6e2
Cristian Sánchez <devcristiansanchez@gmail.com>
2022-02-15 16:03:51 +0100

    feat(convict): add themoviedatabase env variables

--- src/config/environment/index.ts
+++ src/config/environment/index.ts
@@ -7,6 +7,20 @@ const moviedbConfig = convict({
     default: 'default',
     env: 'NODE_ENV',
   },
+  themoviedatabase: {
+    accessToken: {
+      doc: 'The TMDB access token',
+      format: String,
+      env: 'TMDB_ACCESS_TOKEN',
+      default: '',
+    },
+    apiKey: {
+      doc: 'The TMDB api key',
+      format: String,
+      env: 'TMDB_API_KEY',
+      default: '',
+    },
+  },
 });
 
 moviedbConfig.loadFile([`${__dirname}/default.json`, `${__dirname}/${moviedbConfig.get('env')}.json`]);
