cb0eccbfaee16805ac5136e9df88380160805665
Cristian Sánchez <devcristiansanchez@gmail.com>
2022-02-19 14:14:04 +0100

    refactor(mongoose): change MongooseRepository document to set the id to undefined

--- src/modules/shared/persistence/mongoose/MongooseRepository.ts
+++ src/modules/shared/persistence/mongoose/MongooseRepository.ts
@@ -13,7 +13,7 @@ export abstract class MongooseRepository<T> {
   protected async persist(id: string, data: T): Promise<void> {
     const model = await this.model();
 
-    const document = { ...data, _id: id };
+    const document = { ...data, _id: id, id: undefined };
 
     await model.updateOne({ _id: id }, { $set: document }, { upsert: true });
   }
