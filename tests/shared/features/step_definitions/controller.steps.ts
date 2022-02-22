3ac032ed6e928bfa778b929e40845ca21461b170
Cristian Sánchez <devcristiansanchez@gmail.com>
2022-02-22 23:16:38 +0100

    test(cucumber): add put request in controller steps

---
+++ tests/shared/features/step_definitions/controller.steps.ts
@@ -0,0 +1,40 @@
+import { AfterAll, BeforeAll, Given, Then } from '@cucumber/cucumber';
+import { MovieDBBackendApp } from '../../../../src/server/MovieDBBackendApp';
+import request from 'supertest';
+import assert = require('assert');
+import { EnvironmentArranger } from '../../arranger/EnvironmentArranger';
+import container from '../../../../src/config/dependency-injection';
+
+let _request: request.Test;
+let _response: request.Response;
+let application: MovieDBBackendApp;
+
+Given('I send a GET request to {string}', (route: string) => {
+  _request = request(application.httpServer).get(route);
+});
+
+Given('I send a PUT request to {string} with body:', (route: string, body: string) => {
+  _request = request(application.httpServer).put(route).send(JSON.parse(body));
+});
+
+Then('the response status code should be {int}', async (status: number) => {
+  _response = await _request.expect(status);
+});
+
+Then('the response should be empty', () => {
+  assert.deepStrictEqual(_response.body, {});
+});
+
+BeforeAll(async () => {
+  const environmentArranger: Promise<EnvironmentArranger> = container.get('Shared.EnvironmentArranger');
+  await (await environmentArranger).arrange();
+  application = new MovieDBBackendApp();
+  await application.start();
+});
+
+AfterAll(async () => {
+  const environmentArranger: Promise<EnvironmentArranger> = container.get('Shared.EnvironmentArranger');
+  await (await environmentArranger).arrange();
+  await (await environmentArranger).close();
+  await application.stop();
+});
