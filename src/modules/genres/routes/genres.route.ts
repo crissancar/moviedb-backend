f08dcf721cd4633f57627d4a9a985209f42a5dba
Cristian Sánchez <devcristiansanchez@gmail.com>
2022-02-25 13:06:39 +0100

    refactor(routes): add get '/genres/:id' route

--- src/modules/genres/routes/genres.route.ts
+++ src/modules/genres/routes/genres.route.ts
@@ -1,9 +1,12 @@
 import { Request, Response, Router } from 'express';
 import container from '../../../config/dependency-injection';
 import { GenrePutController } from '../controllers/GenrePutController';
+import { GenreGetController } from '../controllers/GenreGetController';
 
 export function register(router: Router) {
   const genrePutController: GenrePutController = container.get('Genres.controllers.GenrePutController');
-
   router.put('/genres/:id', (req: Request, res: Response) => genrePutController.run(req, res));
+
+  const genreGetController: GenreGetController = container.get('Genres.controllers.GenreGetController');
+  router.get('/genres/:id', (req: Request, res: Response) => genreGetController.run(req, res));
 }
