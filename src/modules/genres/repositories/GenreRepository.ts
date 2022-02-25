aa62591d1a24118b3a0ea99aed493f664cea3c71
Cristian Sánchez <devcristiansanchez@gmail.com>
2022-02-25 20:52:39 +0100

    refactor(genre): add search method in the GenreRepository interface

--- src/modules/genres/repositories/GenreRepository.ts
+++ src/modules/genres/repositories/GenreRepository.ts
@@ -1,5 +1,7 @@
 import { Genre } from '../models/Genre';
+import { Nullable } from '../../shared/types/Nullable';
 
 export interface GenreRepository {
   save(genre: Genre): Promise<void>;
+  search(id: string): Promise<Nullable<Genre>>;
 }
