c7f83b32daea9773f3436efea567f248e25c9988
Cristian Sánchez <devcristiansanchez@gmail.com>
2022-02-25 20:53:46 +0100

    refactor(genre): implement search method in InMemoryGenreRepository

--- src/modules/genres/repositories/InMemoryGenreRepository.ts
+++ src/modules/genres/repositories/InMemoryGenreRepository.ts
@@ -1,8 +1,13 @@
 import { GenreRepository } from './GenreRepository';
 import { Genre } from '../models/Genre';
+import { Nullable } from '../../shared/types/Nullable';
 
 export class InMemoryGenreRepository implements GenreRepository {
   async save(genre: Genre): Promise<void> {
     return console.log(genre);
+  }
+
+  async search(id: string): Promise<Nullable<Genre>> {
+    return null;
   }
 }
