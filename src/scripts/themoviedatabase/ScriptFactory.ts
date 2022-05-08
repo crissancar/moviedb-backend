b7202ddd91d16ef269c1780f1ae53037fa060841
Cristian Sánchez <devcristiansanchez@gmail.com>
2022-05-08 22:22:29 +0200

    refactor(scriptfactory): add movies controller

--- src/scripts/themoviedatabase/ScriptFactory.ts
+++ src/scripts/themoviedatabase/ScriptFactory.ts
@@ -1,16 +1,20 @@
 import { TheMovieDatabaseGenresController } from './controllers/genres/TheMovieDatabaseGenresController';
+import { TheMovieDatabaseMoviesController } from './controllers/movies/TheMovieDatabaseMoviesController';
 import container from '../../config/dependency-injection';
 
 export class ScriptFactory {
   private theMovieDatabaseGenresController: TheMovieDatabaseGenresController;
+  private theMovieDatabaseMoviesController: TheMovieDatabaseMoviesController;
 
   constructor(scriptName: string) {
     this.theMovieDatabaseGenresController = container.get('Genres.controllers.TheMovieDatabaseGenresController');
+    this.theMovieDatabaseMoviesController = container.get('Movies.controllers.TheMovieDatabaseMoviesController');
 
     this.runScript(scriptName).then();
   }
 
   public async runScript(scriptName: string) {
     if (scriptName === 'genres') await this.theMovieDatabaseGenresController.run();
+    if (scriptName === 'movies') await this.theMovieDatabaseMoviesController.run();
   }
 }
