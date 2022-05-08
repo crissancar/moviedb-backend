7fb75a06856bd6c95a15f599a704814bb1a1a0e7
Cristian Sánchez <devcristiansanchez@gmail.com>
2022-05-08 22:25:02 +0200

    refactor(tmdb): add constants in enum

--- src/scripts/themoviedatabase/enums/TMDB.ts
+++ src/scripts/themoviedatabase/enums/TMDB.ts
@@ -2,4 +2,7 @@ export enum TMDB {
   API_KEY_URL = '&api_key=',
   GENRES_BASE_URL = 'https://api.themoviedb.org/3/genre/movie/list?language=es-ES',
   IMAGES_BASE_URL = 'https://image.tmdb.org/t/p/',
+  MOVIES_TOTAL_PAGES = 500, // max. 500
+  MOVIES_BASE_URL = 'https://api.themoviedb.org/3/movie/popular?&language=es-ES',
+  MOVIES_PAGE_URL = '&page=',
 }
