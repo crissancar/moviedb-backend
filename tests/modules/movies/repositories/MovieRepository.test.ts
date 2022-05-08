f1c95a65db5698c3e2c13a0f2cbf12c198d37200
Cristian Sánchez <devcristiansanchez@gmail.com>
2022-05-08 23:08:04 +0200

    test(jest): add movie repository test

---
+++ tests/modules/movies/repositories/MovieRepository.test.ts
@@ -0,0 +1,41 @@
+import { MovieRepository } from '../../../../src/modules/movies/repositories/MovieRepository';
+import container from '../../../../src/config/dependency-injection';
+import { EnvironmentArranger } from '../../../shared/arranger/EnvironmentArranger';
+import { MovieMother } from '../mothers/MovieMother';
+
+const repository: MovieRepository = container.get('Movie.repositories.MovieRepository');
+const environmentArranger: Promise<EnvironmentArranger> = container.get('Shared.EnvironmentArranger');
+
+beforeEach(async () => {
+  await (await environmentArranger).arrange();
+});
+
+afterAll(async () => {
+  await (await environmentArranger).arrange();
+  await (await environmentArranger).close();
+});
+
+describe('MovieRepository', () => {
+  describe('#save', () => {
+    it('should save a movie', async () => {
+      const movie = MovieMother.random();
+
+      await repository.save(movie);
+    });
+  });
+
+  describe('#search', () => {
+    it('should return an existing movie', async () => {
+      const expectedMovie = MovieMother.random();
+      await repository.save(expectedMovie);
+
+      const findedMovie = await repository.search(expectedMovie.id);
+
+      expect(expectedMovie).toEqual(findedMovie);
+    });
+
+    it('should not return a non existing course', async () => {
+      expect(await repository.search(MovieMother.random().id)).toBeFalsy();
+    });
+  });
+});
